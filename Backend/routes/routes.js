f0e0baa66ac23a98c528b37549915984065e720a
Kaleb Washington <kdwashington302@gmail.com>
2021-11-18 14:21:46 -0700

    connected mongo

--- Backend/routes/routes.js
+++ Backend/routes/routes.js
@@ -1,5 +1,5 @@
 const { MongoClient, ObjectId } = require('mongodb');
-const url = '';
+const url = 'mongodb+srv://group:MTM282@dataexpressdb.108zd.mongodb.net/DataExpress?retryWrites=true&w=majority';
 const client = new MongoClient(url);
 
 exports.index = (req, res) => {
